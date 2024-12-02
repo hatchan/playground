692ee18be489804eb09b2845657b7d08aa479715
Benno van den Berg <bennovandenberg@gmail.com>
2024-12-02 14:30:23 +0100

    Solve Day 2a

--- aoc/2024/02/src/main.rs
+++ aoc/2024/02/src/main.rs
@@ -6,10 +6,31 @@ fn main() {
 }
 
 fn solve_a(input: &str) -> usize {
-    todo!()
+    let input = parse_input(input);
+
+    input.into_iter().filter(is_gradual).count()
 }
 
-fn solve_b(input: &str) -> usize {
+fn is_gradual(input: &Vec<isize>) -> bool {
+    let diffs: Vec<_> = input.windows(2).map(|a| a[1] - a[0]).collect();
+
+    let sign_all_same = diffs.iter().all(|&diff| diff > 0) || diffs.iter().all(|&diff| diff < 0);
+
+    if !sign_all_same {
+        return false;
+    }
+
+    diffs.iter().all(|&diff| diff.abs() > 0 && diff.abs() <= 3)
+}
+
+fn parse_input(input: &str) -> Vec<Vec<isize>> {
+    input
+        .lines()
+        .map(|line| line.split(' ').map(|n| n.parse().unwrap()).collect())
+        .collect()
+}
+
+fn solve_b(input: &str) -> isize {
     todo!()
 }
 
@@ -22,7 +43,7 @@ mod tests {
     #[test]
     fn example_a() {
         let result = solve_a(EXAMPLE_INPUT);
-        assert_eq!(result, 11);
+        assert_eq!(result, 2);
     }
 
     // #[test]
