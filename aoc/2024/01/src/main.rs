3e8b123592abde18687f96992b49583b4f630957
Benno van den Berg <bennovandenberg@gmail.com>
2024-12-02 13:29:55 +0100

    Solve Day 1b

--- aoc/2024/01/src/main.rs
+++ aoc/2024/01/src/main.rs
@@ -1,11 +1,14 @@
+use std::collections::HashMap;
+
 const INPUT: &'static str = include_str!("../input.txt");
 
 fn main() {
     println!("Day 1a: {}", solve_a(INPUT));
+    println!("Day 1b: {}", solve_b(INPUT));
 }
 
 fn solve_a(input: &str) -> usize {
-    let (mut left, mut right) = parse(input);
+    let (mut left, mut right) = parse_a(input);
 
     left.sort_unstable();
     right.sort_unstable();
@@ -19,7 +22,7 @@ fn solve_a(input: &str) -> usize {
     result
 }
 
-fn parse(input: &str) -> (Vec<usize>, Vec<usize>) {
+fn parse_a(input: &str) -> (Vec<usize>, Vec<usize>) {
     let mut left_result = Vec::new();
     let mut right_result = Vec::new();
 
@@ -34,6 +37,29 @@ fn parse(input: &str) -> (Vec<usize>, Vec<usize>) {
     (left_result, right_result)
 }
 
+fn solve_b(input: &str) -> usize {
+    let (left, right) = parse_b(input);
+
+    let result = left
+        .into_iter()
+        .map(|left| left * right.get(&left).copied().unwrap_or_default())
+        .sum();
+
+    result
+}
+
+fn parse_b(input: &str) -> (Vec<usize>, HashMap<usize, usize>) {
+    let (left, right) = parse_a(input);
+
+    let right_totals: HashMap<usize, usize> =
+        right.into_iter().fold(HashMap::new(), |mut acc, x| {
+            acc.entry(x).and_modify(|entry| *entry += 1).or_insert(1);
+            acc
+        });
+
+    (left, right_totals)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -44,5 +70,11 @@ mod tests {
     fn example_a() {
         let result = solve_a(EXAMPLE_INPUT);
         assert_eq!(result, 11);
+    }
+
+    #[test]
+    fn example_b() {
+        let result = solve_b(EXAMPLE_INPUT);
+        assert_eq!(result, 31);
     }
 }
